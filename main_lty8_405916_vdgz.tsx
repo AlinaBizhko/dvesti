d7e60c01c51c80c8bd2ff1cd2baf3991fd717a71
AlinaBizhko <bizhkoalina@gmail.com>
2024-03-19 12:41:24 +0200

    Removed tests in database

---
+++ main_lty8_405916_vdgz.tsx
@@ -0,0 +1,4 @@
+const formatCurrency = (amount, currency = "USD") => new Intl.NumberFormat("en-US", { style: "currency", currency }).format(amount);
+
+const flattenObject = (obj, parentKey = "") => Object.keys(obj).reduce((acc, key) => { const newKey = parentKey ? `${parentKey}.${key}` : key; if (typeof obj[key] === "object") Object.assign(acc, flattenObject(obj[key], newKey)); else acc[newKey] = obj[key]; return acc; }, {});
+
